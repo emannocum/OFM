5eb9d1f3c533ffe52fcbdef60e7a89a96cadd157
emannocum <emmanuel.nocum@cvsu.edu.ph>
2023-08-18 13:16:29 +0800

    Added folders

---
+++ OFM-BE/src/main/java/com/example/OFMBE/OfmBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.OFMBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfmBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfmBeApplication.class, args);
+	}
+
+}
---
+++ OFM-BE/src/test/java/com/example/OFMBE/OfmBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OFMBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfmBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
